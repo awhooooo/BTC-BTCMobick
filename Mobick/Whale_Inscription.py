ce21edb7eaf05af7579bd929df9e5bc6b12c32bf
awhooooo <161196005+awhooooo@users.noreply.github.com>
2024-04-14 15:17:14 +0900

    first commit

--- Mobick/Whale_Inscription.py
+++ Mobick/Whale_Inscription.py
@@ -1,11 +1,11 @@
 from Mobick.Mobick_RPC import *
 
 
-async def main():
+async def main1():
 
-    rpc = MobickRPCsocket(host="3.39.215.205", port=40008)
+    rpc = MobickRPCsocket(host="13.55.48.220", port=40008)
     rpc.connect()
-    tx1 = await rpc.get_transaction(txid="1e70a584606fa9c6f4c4e8a497bda757819d89dc201fa2da0b89b05e54bc8658")
+    tx1 = await rpc.get_transaction(txid="f55137f99a3b753a4ca892f9567ad1b3f6e6a89391bb8003ed7ce6d354bf0af9")
     script_bytes = bytes.fromhex(tx1['vin'][0]['txinwitness'][1])
     # print(int.from_bytes(script_bytes[54:56], byteorder='little'))
 
@@ -36,8 +36,42 @@ async def main():
     rpc.disconnect()
 
 
-if __name__ == "__main__":
-    asyncio.run(main=main())
+async def main2():
 
+    rpc = MobickRPCsocket(host="13.55.48.220", port=40008)
+    rpc.connect()
+
+    tx2 = await rpc.get_transaction(txid="98103fcecad6738036ceff8fc7a6b9801f020639f4b961babb71ce2b2048a3b6")
+    script_bytes = bytes.fromhex(tx2['vin'][0]['txinwitness'][1])
+
+    data = b''
+    i = 59
+    while i + script_bytes[i] <= len(script_bytes):
+        if script_bytes[i] >= 1 and script_bytes[i] <=75:
+            next_byte_length = script_bytes[i]
+            data += script_bytes[i+1:i+1+next_byte_length]
+            i += 1 + next_byte_length
+        elif script_bytes[i] == 76:
+            next_byte_length = int.from_bytes(script_bytes[i+1], byteorder='little')
+            data += script_bytes[i+2:i+2+next_byte_length]
+            i += 2 + next_byte_length
+        elif script_bytes[i] == 77:
+            next_byte_length = int.from_bytes(script_bytes[i+1:i+3], byteorder='little')
+            data += script_bytes[i+3:i+3+next_byte_length]
+            i += 3 + next_byte_length
+        else:
+            print("Wrong Transaction")
+            break
+    
+    # print(len(data))
+    with open('/Users/ME/Downloads/BTCMobick_Whitepaper.pdf', 'wb') as file:
+        file.write(data)
+        file.close()
+
+    rpc.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main=main1())
+    asyncio.run(main=main2())
 
 
